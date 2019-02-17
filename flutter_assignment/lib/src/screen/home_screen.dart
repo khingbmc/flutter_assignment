de5d723b630c184328ced39d5c320d314de37cb7
khingbmc <ratanon.jan@gmail.com>
2019-02-17 19:05:08 +0700

    create home screen

--- flutter_assignment/lib/src/screen/home_screen.dart
+++ flutter_assignment/lib/src/screen/home_screen.dart
@@ -8,23 +8,58 @@ class HomeScreen extends StatefulWidget{
   }
 }
 
-class HomeScreenState extends State<HomeScreen>{
+class HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin{
  
   Widget build(BuildContext context) {
+    int index = 0;
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("This is Third Screen"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("Home"),
+          ),
+          body: TabBarView(
+            controller: ,
+          ),
+        ),
       ),
-      body: Center(
-        child: FlatButton(
-          child: Text("Back First Screen"),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        )
-        ,),
     );
   }
+
+  List<Widget> tab = [
+    Center(
+      child: Text("Home",
+      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 80.0),),
+      
+    ),
+    Center(child: Text("Notify",
+    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 80.0),),
+    
+    ),
+    Center(
+      child: Text("Map",
+    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 80.0),),
+    
+    ),
+    Center(
+      child: Text("Profile",
+    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 80.0),),
+    
+    ),
+    Center(
+      child: Text("Setup",
+    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 80.0),),
+    
+    ),
+  ];
+
+  TabController tabControling;
+
+  
+  
 
 }
--- flutter_assignment/lib/src/screen/register_screen.dart
+++ flutter_assignment/lib/src/screen/register_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'login_screen.dart';
 
 class RegistScreen extends StatefulWidget{
   @override
@@ -9,22 +11,114 @@ class RegistScreen extends StatefulWidget{
 }
 
 class RegistScreenState extends State<RegistScreen>{
- 
+  TextEditingController user_checking = new TextEditingController();
+  TextEditingController password_checking = new TextEditingController();
+  TextEditingController repassword_checking = new TextEditingController();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
   Widget build(BuildContext context) {
+
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("This is Second Screen"),
+    return Center(
+      child:Scaffold(
+        resizeToAvoidBottomPadding: false,
+        key: scaffoldKey,
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+        title: Container(
+          padding: EdgeInsets.fromLTRB(60.0, 0.0, 0.0, 0.0),
+          child: Text("REGISTER"),
+        ),
       ),
-      body: Center(
-        child: FlatButton(
-          child: Text("Back First Screen"),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        )
-        ,),
+      body: Container(
+      margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 10.0),
+                padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
+      child: Center(
+        child: Form(
+          child: Column(
+            children:<Widget> [
+              emailField(),
+              passwordField(),
+              repasswordField(),
+              submitBtnField()
+          ],
+        ),
+      )
+      ),
+      
+    ),
+    )
     );
+  }
+Widget emailField(){
+    return Container(
+      
+      padding: EdgeInsets.only(top: 40.0),
+      child: TextFormField(
+        
+        controller: user_checking,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+        prefixIcon: Icon(Icons.mail),
+        hintText: 'User Id',
+      ),
+    ),
+    );
+  }
+
+  Widget passwordField(){
+    return Container(
+     
+      padding: EdgeInsets.only(top: 10.0),
+      child: TextFormField(
+        controller: password_checking,
+        decoration: InputDecoration(
+        prefixIcon: Icon(Icons.lock),
+        hintText: 'Password',
+      ),
+      obscureText: true,
+    ),
+    );
+  }
+
+  Widget repasswordField(){
+    return Container(
+     
+      padding: EdgeInsets.only(top: 10.0),
+      child: TextFormField(
+        controller: repassword_checking,
+        decoration: InputDecoration(
+        prefixIcon: Icon(Icons.lock),
+        hintText: 'Re-Password',
+      ),
+      obscureText: true,
+    ),
+    );
+  }
+
+  Widget submitBtnField(){
+    return Container(
+      padding: EdgeInsets.only(top: 25.0),
+          child: ButtonTheme(
+            minWidth: 325,
+            child: RaisedButton(
+              textColor: Colors.white,
+              color: Colors.pink.shade500,
+              child: Text('Continue'),
+              onPressed: (){
+                if(user_checking.text.isEmpty || password_checking.text.isEmpty || repassword_checking.text.isEmpty){
+                  scaffoldKey.currentState.showSnackBar(SnackBar(
+                    content: Text('กรุณาระบุข้อมูลให้ครบถ้วน'),));
+                }else if(user_checking.text == "admin"){
+                  scaffoldKey.currentState.showSnackBar(SnackBar(
+                    content: Text('user นี้มีอยู่ในระบบแล้ว'),));
+                }else{
+                  Navigator.push(context, 
+                  MaterialPageRoute(builder: (context) => LoginScreen()));
+                }
+              },
+        ),
+          ),
+      );
   }
 
 }
